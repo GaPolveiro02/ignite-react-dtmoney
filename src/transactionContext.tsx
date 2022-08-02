b76c09646a1a0215475e4ece1c574c620490f30c
GaPolveiro02 <gabrielpolveiro12@gmail.com>
2022-08-02 18:03:39 -0300

    Calculating summary

--- src/transactionContext.tsx
+++ src/transactionContext.tsx
@@ -18,12 +18,12 @@ interface TransactionProviderProps {
 
 interface TransactionsContextData {
     transactions: Transaction[];
-    createTransaction: (transaction: TransactionInput) => void;
+    createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
-)
+);
 
 export function TransactionsProvider({children}: TransactionProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -31,10 +31,19 @@ export function TransactionsProvider({children}: TransactionProviderProps) {
     useEffect(() => {
         api.get('/transactions')
         .then(response => setTransactions(response.data.transactions))
-    }, [])
+    }, []);
 
-    function createTransaction(transaction: TransactionInput) {
-        api.post('/transactions', transaction)
+    async function createTransaction(transactionInput: TransactionInput) {
+        const response = await api.post('/transactions', {
+            ...transactionInput,
+            createdAt: new Date()
+        })
+        const { transaction } = response.data;
+
+        setTransactions([
+            ...transactions,
+            transaction,
+        ]);
     }
 
     return (
